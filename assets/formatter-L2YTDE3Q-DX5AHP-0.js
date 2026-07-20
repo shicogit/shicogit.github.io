e47121ace0981d5007ab6da07f9b8c0b569ba762
shicogit <shira.giladi@xero.com>
2026-07-20 10:43:17 -0400

    Publish Storybook

---
+++ assets/formatter-L2YTDE3Q-DX5AHP-0.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{N as t,P as n}from"./iframe-pyJnZA3T.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-DYLaPElv.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
